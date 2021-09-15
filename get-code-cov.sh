36ed7e121ac5a321ff938c3dd18171d85a607644
Juho Röyskö <juho.roysko@paivola.fi>
2021-09-15 17:28:37 +0300

    Remove tests from code coverage

--- get-code-cov.sh
+++ get-code-cov.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-for filename in `find tests -name "*\.cpp"`; 
-do 
-  gcov -n -o build-files/tests/obj/tests $filename > /dev/null; 
+
+# Remove code coverage for test files.
+for filename in `find build-files/tests/obj/tests/ -name "*\.gcda"`;
+do
+    rm $filename
+done
+for filename in `find build-files/tests/obj/tests/ -name "*\.gcno"`;
+do
+    rm $filename
 done
 
-for filename in `find src -name "*\.cpp" | grep -v "main.cpp"`; 
-do 
-  gcov -n -o build-files/tests/obj/src $filename > /dev/null; 
+# Get code coverage
+for filename in `find src -name "*\.cpp" | grep -v "main.cpp"`;
+do
+    gcov -n -o build-files/tests/obj/src $filename > /dev/null;
 done
